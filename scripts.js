7f8047a71a21c4f4825b9fd2a32872e1023765b1
alicefritz <alicejfritz@gmail.com>
2021-02-22 21:22:30 +0100

    added basic fetching of items

--- scripts.js
+++ scripts.js
@@ -1,1 +1,9 @@
 console.log('hello world')
+
+const getItems = (category) => {
+  fetch(`https://bad-api-assignment.reaktor.com/v2/products/${category}`)
+  .then(res => res.json())
+  .then(data => {
+    console.log(data)
+  })
+}
